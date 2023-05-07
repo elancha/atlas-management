19b3fe7bed620d57cc945974884fc11005b17e8d
elancha <estherlancha@gmail.com>
2023-05-07 20:01:05 +0200

    Modificacion de Login

--- src/App.js
+++ src/App.js
@@ -1,53 +1,11 @@
-/* eslint-disable react/prop-types */
-import React, { Component, Suspense } from 'react'
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './scss/style.scss'
 
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse"></div>
-  </div>
-)
-
-const RequireAuth = ({ children }) => {
-  return <Navigate to="/login" replace /> //Esto hace que siempre me devuelva el login
-  if (children) {
-    return children
-  }
-}
-
-// Containers
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
-
-// Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+import React, { Component } from 'react'
+import AppRouter from './components/AppRouter'
 
 class App extends Component {
   render() {
-    return (
-      <HashRouter>
-        <Suspense fallback={loading}>
-          <Routes>
-            <Route
-              path="*"
-              name="Home"
-              element={
-                <RequireAuth>
-                  <DefaultLayout />
-                </RequireAuth>
-              }
-            />
-            <Route exact path="/login" name="Login Page" element={<Login />} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
-            <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          </Routes>
-        </Suspense>
-      </HashRouter>
-    )
+    return <AppRouter />
   }
 }
 
---
+++ src/views/Login.jsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation, useSearchParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CRow,
+  CSpinner,
+} from '@coreui/react';
+
+import Button from 'react-bootstrap/esm/Button';
+import Feedback from 'react-bootstrap/esm/Feedback';
+import FormLabel from 'react-bootstrap/esm/FormLabel';
+import FormGroup from 'react-bootstrap/esm/FormGroup';
+import InputGroup from 'react-bootstrap/esm/InputGroup';
+import FormControl from 'react-bootstrap/esm/FormControl';
+
+import Form from 'react-bootstrap/esm/Form';
+import { Person, Lock } from 'react-bootstrap-icons';
+
+import { useFormik } from 'formik';
+import { object, string } from 'yup';
+import { getToken, setToken } from 'src/store/appSlice';
+import {
+  getUser,
+} from 'src/store/appSlice';
+
+export default function Login() {
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const user = useSelector(getUser);
+  const token = useSelector(getToken);
+  const dispatch = useDispatch();
+  const [urlParams] = useSearchParams();
+  const [authCode, setAuthCode] = useState("");
+
+  const validationSchema = object().shape({
+    email: string().default(urlParams.get("email") || "").required().matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i),
+    password: string().default('').required(),
+  });
+
+  useEffect(() => {
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+        // Try to go back to the place the user was before we redirected
+        // him/her to the /login
+        navigate(location.state?.from?.pathname || '/');
+      }
+  }, [token]);
+
+  const formik = useFormik({
+    initialValues: validationSchema.getDefault(),
+    validationSchema,
+    onSubmit: async (values) => {
+      dispatch(setToken({accessToken: "123456"}));
+
+      formik.setSubmitting(false);
+    },
+  });
+
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={6}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <Form noValidate onSubmit={formik.handleSubmit}>
+                    <div className='row'>
+                      <div className='col-6'>
+                        <div className='row'>
+                          <div className='col-12'>
+                            <h1>Login</h1>
+                            <p className="text-medium-emphasis">Sign in to your account</p>
+                          </div>
+
+                          <div className='col-12 mb-3'>
+                            <div className="input-group">
+                              <FormGroup>
+                                <FormLabel className="col-form-label">Email</FormLabel>
+                                <InputGroup className={formik.touched.email && formik.errors.email && 'is-invalid'}>
+                                  <InputGroup.Text><Person size={20} /></InputGroup.Text>
+                                  <FormControl
+                                    id="email"
+                                    placeholder="Type your email"
+                                    type="text"
+                                    autoComplete="username"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.email}
+                                    isInvalid={formik.touched.email && formik.errors.email}
+                                  />
+                                </InputGroup>
+                                {
+                                  formik.errors.email ? (
+                                    <Feedback type="invalid">{formik.errors.email}</Feedback>
+                                  ) : ""
+                                }
+                              </FormGroup>
+                            </div>
+                          </div>
+
+                          <div className='col-12 mb-3'>
+                            <div className="input-group">
+                              <FormGroup>
+                                <FormLabel className="col-form-label">Password</FormLabel>
+                                <InputGroup className={formik.touched.password && formik.errors.password && 'is-invalid'}>
+                                  <InputGroup.Text><Lock size={20} /></InputGroup.Text>
+                                  <FormControl
+                                    id="password"
+                                    placeholder="Type the password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                    isInvalid={formik.touched.password && formik.errors.password}
+                                  />
+                                </InputGroup>
+                                {
+                                  formik.errors.password ? (
+                                    <Feedback type="invalid">{formik.errors.password}</Feedback>
+                                  ) : ""
+                                }
+                              </FormGroup>
+                            </div>
+                          </div>
+
+                          <div className='col-12 mt-4'>
+                            <div className='row'>
+                              <div className='col-2 me-3'>
+                                <Button disabled={formik.isSubmitting} variant="primary" className="px-4" type="submit">
+                                  {
+                                    formik.isSubmitting && <CSpinner size="sm" className='me-2' />
+                                  }
+                                  Login
+                                </Button>
+                              </div>
+
+                              <div className='col-auto d-flex flex-column justify-content-center'>
+                                <span className="text-danger">{message}</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className='col-6 d-flex'>
+                        <div className='row ms-3 ps-3 h-100 align-items-center border-start color-secondary'>
+                          <div className='col-12'>
+                            <span className='d-block mb-3'>Having problems with your credentials?</span>
+                            <a onClick={() => navigate(`/recover${formik.values.email ? `?email=${formik.values.email}` : ""}`)} className="pointer text-medium-emphasis">Reset your password</a>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>                 
+                  </Form>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  );
+}
