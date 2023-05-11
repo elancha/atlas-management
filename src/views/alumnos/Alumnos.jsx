fdbd956b241dac64b684ad2637a75ff3a3cbffaf
elancha <estherlancha@gmail.com>
2023-05-11 16:45:19 +0200

    Generate table

--- src/views/alumnos/Alumnos.jsx
+++ src/views/alumnos/Alumnos.jsx
@@ -1,9 +1,40 @@
+import { CCard, CCardBody, CCardHeader, CTable, CTableBody, CTableHead, CTableHeaderCell, CTableRow } from '@coreui/react'
 import React from 'react'
 
 const Alumnos = () => {
     return (
         <>
-            ALUMNOS
+            <CCard>
+                <CCardHeader align="middle">
+                    <strong>Lista de Alumnos</strong>
+                </CCardHeader>
+                <CCardBody>
+                    <CTable hover>
+                        <CTableHead>
+                            <CTableRow>
+                                <CTableHeaderCell scope='col' className='w-auto'>
+                                    Nombre
+                                </CTableHeaderCell>
+                                <CTableHeaderCell scope='col' className='w-auto'>
+                                    Apellidos
+                                </CTableHeaderCell>
+                                <CTableHeaderCell scope='col' className='w-auto'>
+                                    DNI
+                                </CTableHeaderCell>
+                                <CTableHeaderCell scope='col' className='w-auto'>
+                                    Fecha de Nacimiento
+                                </CTableHeaderCell>
+                                <CTableHeaderCell scope='col' className='w-auto'>
+                                    Clan Principal
+                                </CTableHeaderCell>
+                            </CTableRow>
+                        </CTableHead>
+                        <CTableBody>
+                            
+                        </CTableBody>
+                    </CTable>
+                </CCardBody>
+            </CCard>
         </>
     )
 }
